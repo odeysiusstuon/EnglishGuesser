5f25f03784f77ee17e24295cef159a8f8b014787
therealvidem <superninario@gmail.com>
2022-04-25 10:17:01 -0700

    Started on layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,21 @@
-<h1>English Guesser</h1>
+<script lang="ts">
+    import Slider from '$lib/slider.svelte';
+</script>
+
+<div style="grid grid-cols-6 gap-4" id="main">
+    <div class="col-start-2 col-span-4">
+        <h1 class="text-3xl font-bold underline">
+            History of English Guesser
+        </h1>
+    </div>
+    
+    <div class="col-span-1">
+        <Slider />
+    </div>
+</div>
+
+<style>
+    #main {
+        height: 100%;
+    }
+</style>
