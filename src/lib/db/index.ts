deabe63f2263c0d594513483f5921f80a934f49b
odeysiusstuon <odeysiusstuon@gmail.com>
2022-05-08 18:33:39 -0700

    Readded dotenv

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,10 +1,11 @@
 import type { IDatabase } from "$lib/types";
 import { MongoClient } from "mongodb";
 import { MongoDB } from "./mongodb";
-import 'dotenv/config';
 import { test_database } from "./test_database";
 
-const uri = process.env.MONGODB_URI;
+import { env } from '$lib/env';
+
+const uri = env.MONGODB_URI;
 
 if (!uri) {
   throw new Error('Could not find URI');
